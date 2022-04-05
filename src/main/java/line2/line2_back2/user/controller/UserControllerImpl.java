54d6821a16fd6a59cda2dea4e95403b30a0de43f
hd <gudehdu10@gmail.com>
2022-04-05 16:48:08 +0900

    msa test

--- src/main/java/line2/line2_back2/user/controller/UserControllerImpl.java
+++ src/main/java/line2/line2_back2/user/controller/UserControllerImpl.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-@RequestMapping("/manage")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 @CrossOrigin
 public class UserControllerImpl implements UserController {
